001cff0e380497e28ca61f476a5e21a445986c7a
eb0687 <ebrahim0687@gmail.com>
2022-09-25 00:31:20 +0300

    added .bashtools

---
+++ .bashtools/.alias.bash
@@ -0,0 +1,27 @@
+# general
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+alias clear='clear -x'
+alias cl='clear -x'
+alias vi='vim'
+alias v='nvim'
+alias reload='source ~/.bashrc'
+alias f='fzf'
+alias follow='tail -f -n +1'
+
+# cd
+alias ..='cd ..'
+
+# git
+alias g='git'
+alias gs='git status'
+alias ga='git add'
+alias gcom='git commit -v'
+alias gp='git push'
+
+#tmux
+alias tm='tmat'
+alias tma='tmux attach'
+alias tmd='tmux detach'
+# alias tmls='tmux list-session'
