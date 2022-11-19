80f673a8c396d2ff8c6ea6813c267ffdcbbf2979
eb0687 <ebrahim0687@gmail.com>
2022-11-19 16:12:31 +0300

    added new function ansible-role-fd

--- .bashtools/.functions.bash
+++ .bashtools/.functions.bash
@@ -29,3 +29,20 @@ function gcoma () {
     git add .
     git commit -m "$1"
 }
+
+ansible-role-fd() {
+    local ANSIBLE_ROLE_DIR="$HOME/.ansible/roles/"
+    local FD_RESULT=$(fdfind . $ANSIBLE_ROLE_DIR | fzf --header='Ansible Roles:')
+    echo $ANSIBLE_DIR
+
+    if [[ -d $ANSIBLE_ROLE_DIR ]]; then
+        if [[ -d $FD_RESULT ]]; then
+            cd $FD_RESULT
+        elif [[ -f $FD_RESULT ]]; then
+            /usr/bin/nvim $FD_RESULT
+        fi
+    else
+        echo -e "\n$ANSIBLE_ROLE_DIR does not exist, are you sure you have ansible installed on this device?"
+        echo -e "\nCheck using the command: whereis ansible"
+    fi
+}
